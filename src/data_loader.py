4aa63a333c4f5119f5806bbb9f2087430308ced4
kubra-ps1 <k97319882@gmail.com>
2025-09-19 00:05:07 +0300

    olası bütün datalrın kullanılması için bütün fonksiyonlar tamamlandı

--- src/data_loader.py
+++ src/data_loader.py
@@ -4,4 +4,8 @@ def load_latest_prices(csv_path=r"C:\Users\skubra\smart-market-optimizer\data\ma
     df=pd.read_csv(csv_path)
     son_tarih=df["tarih"].max()
     df_son_gun=df[df["tarih"]==son_tarih]
-    return df_son_gun
+    return df_son_gun,son_tarih
+
+def load_data(csv_path=r"C:\Users\skubra\smart-market-optimizer\data\market_prices.csv"):
+    df=pd.read_csv(csv_path)
+    return df
